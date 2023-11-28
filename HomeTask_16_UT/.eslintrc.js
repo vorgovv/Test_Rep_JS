a1e6e20d4b0a65df4fb311d85e0dda0abbec1447
Svyatoslav Yakornov <yakornovml@gmail.com>
2023-11-28 19:04:46 +0300

    calc

---
+++ HomeTask_16_UT/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+      mocha: true,
+      node: true,
+    },
+    extends: 'eslint:recommended',
+    rules: {
+      
+    },
+    ignorePatterns: ['node_modules/', 'dist/'],
+  };
